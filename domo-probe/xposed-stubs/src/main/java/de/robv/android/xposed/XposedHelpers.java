47c4f7913e5ea05bc992ae8f4ffad6ce5a753bde
Axymorrsen <Axymosen@outlook.com>
2026-07-16 13:41:14 +0800

    domo probe: add XposedHelpers stub

---
+++ domo-probe/xposed-stubs/src/main/java/de/robv/android/xposed/XposedHelpers.java
@@ -0,0 +1,10 @@
+package de.robv.android.xposed;
+
+public final class XposedHelpers {
+    private XposedHelpers() {}
+
+    public static Class<?> findClassIfExists(String className, ClassLoader classLoader) { return null; }
+    public static Object callMethod(Object obj, String methodName, Object... args) { return null; }
+    public static Object callStaticMethod(Class<?> clazz, String methodName, Object... args) { return null; }
+    public static Object getObjectField(Object obj, String fieldName) { return null; }
+}
